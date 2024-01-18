2fef268fdbc503c4fe67404a147c02980d36d03d
mrrust77 <felixmartel.no@gmail.com>
2024-01-18 23:34:06 +0100

    refactor codebase

---
+++ programs/staking/src/instructions.rs
@@ -0,0 +1,9 @@
+mod deposit_funds;
+mod initialize;
+mod withdraw_funds;
+mod withdrawal_request;
+
+pub use deposit_funds::*;
+pub use initialize::*;
+pub use withdraw_funds::*;
+pub use withdrawal_request::*;
