b350961db71b44b5cd8dedd5ec731b11c5c335e1
milkomeda77 <felixmartel.no@gmail.com>
2024-05-09 01:08:56 +0900

    feat: refactor withdraw/deposit contract code

--- programs/staking/src/instructions/deposit_funds.rs
+++ programs/staking/src/instructions/deposit_funds.rs
@@ -1,41 +1,36 @@
 use crate::state::State;
 use crate::utils::calculate_rewards;
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
+use anchor_spl::token::{self, Mint, TokenAccount, Transfer};
 
 #[derive(Accounts)]
 #[instruction(application_idx: u64, state_bump: u8, wallet_bump: u8)]
 pub struct Deposit<'info> {
     #[account(
         mut,
-        seeds = [b"state".as_ref(),
-                 user_sending.key().as_ref(),
-                 mint_of_token_being_sent.key().as_ref(),
-                 application_idx.to_le_bytes().as_ref()],
+        seeds = [b"state".as_ref(), user_sending.key().as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
         bump = state_bump
     )]
     application_state: Account<'info, State>,
     #[account(
         mut,
-        seeds = [b"wallet".as_ref(),
-                mint_of_token_being_sent.key().as_ref(),
-                application_idx.to_le_bytes().as_ref()],
+        seeds = [b"wallet".as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
         bump = wallet_bump,
-        token::mint=mint_of_token_being_sent,
-        token::authority=application_state,
+        token::mint = mint_of_token_being_sent,
+        token::authority = application_state,
     )]
     escrow_wallet_state: Account<'info, TokenAccount>,
-    #[account(mut)]
-    user_sending: Signer<'info>,
+    #[account(signer)]
+    user_sending: AccountInfo<'info>,
     mint_of_token_being_sent: Account<'info, Mint>,
     #[account(
         mut,
-        constraint=wallet_to_withdraw_from.owner == user_sending.key(),
-        constraint=wallet_to_withdraw_from.mint == mint_of_token_being_sent.key()
+        constraint = wallet_to_withdraw_from.owner == *user_sending.key(),
+        constraint = wallet_to_withdraw_from.mint == mint_of_token_being_sent.key()
     )]
     wallet_to_withdraw_from: Account<'info, TokenAccount>,
-    system_program: Program<'info, System>,
-    token_program: Program<'info, Token>,
+    system_program: AccountInfo<'info>,
+    token_program: AccountInfo<'info>,
     rent: Sysvar<'info, Rent>,
 }
 
@@ -43,70 +38,42 @@ pub fn deposit_funds_handler(
     ctx: Context<Deposit>,
     application_idx: u64,
     state_bump: u8,
-    _wallet_bump: u8,
+    wallet_bump: u8,
     amount: u64,
     timestamp: u64,
     unbounding_period: u64,
 ) -> Result<()> {
     let state = &mut ctx.accounts.application_state;
     state.idx = application_idx;
-    state.user_sending = ctx.accounts.user_sending.key().clone();
-    state.mint_of_token_being_sent = ctx.accounts.mint_of_token_being_sent.key().clone();
-    state.escrow_wallet = ctx.accounts.escrow_wallet_state.key().clone();
+    state.user_sending = *ctx.accounts.user_sending.key();
+    state.mint_of_token_being_sent = *ctx.accounts.mint_of_token_being_sent.key();
+    state.escrow_wallet = *ctx.accounts.escrow_wallet_state.to_account_info().key;
     state.unbounding_period_in_days = unbounding_period;
 
-    if state.amount_tokens == 0 {
-        state.amount_tokens = amount;
-        state.last_deposit_timestamp = timestamp;
-    } else {
-        state.amount_tokens = amount
-            + calculate_rewards(
-                state.amount_tokens,
-                state.last_deposit_timestamp,
-                timestamp,
-                0,
-            );
-        state.last_deposit_timestamp = timestamp;
+    let mut amount_with_rewards = amount;
+    if state.amount_tokens != 0 {
+        amount_with_rewards += calculate_rewards(
+            state.amount_tokens,
+            state.last_deposit_timestamp,
+            timestamp,
+            0,
+        );
     }
+    state.amount_tokens = amount_with_rewards;
+    state.last_deposit_timestamp = timestamp;
 
     msg!("Initialized new Safe Transfer instance for {}", amount);
 
-    // CPI time! we now need to call into the Token program to transfer our funds to the
-    // Escrow wallet. Our state account account is a PDA, which means that no private key
-    // exists for the corresponding public key and therefore this key was not signed in the original
-    // transaction. Our program is the only entity that can programmatically sign for the PDA
-    // and we can do this by specifying the PDA "derivation hash key" and using `CpiContext::new_with_signer()`.
-
-    // This specific step is very different compared to Ethereum. In Ethereum, accounts need to first set allowances towards
-    // a specific contract (like ZeroEx, Uniswap, Curve..) before the contract is able to withdraw funds. In this other case,
-    // the SafePay program can use Bob's signature to "authenticate" the `transfer()` instruction sent to the token contract.
-    let bump_vector = state_bump.to_le_bytes();
-    let mint_of_token_being_sent_pk = ctx.accounts.mint_of_token_being_sent.key().clone();
-    let application_idx_bytes = application_idx.to_le_bytes();
-    let inner = vec![
-        b"state".as_ref(),
-        ctx.accounts.user_sending.key.as_ref(),
-        mint_of_token_being_sent_pk.as_ref(),
-        application_idx_bytes.as_ref(),
-        bump_vector.as_ref(),
-    ];
-    let outer = vec![inner.as_slice()];
-
-    // Below is the actual instruction that we are going to send to the Token program.
-    let transfer_instruction = Transfer {
-        from: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
-        to: ctx.accounts.escrow_wallet_state.to_account_info(),
-        authority: ctx.accounts.user_sending.to_account_info(),
-    };
-    let cpi_ctx = CpiContext::new_with_signer(
-        ctx.accounts.token_program.to_account_info(),
-        transfer_instruction,
-        outer.as_slice(),
-    );
-
-    // The `?` at the end will cause the function to return early in case of an error.
-    // This pattern is common in Rust.
-    anchor_spl::token::transfer(cpi_ctx, amount)?;
+    // Transfer tokens from user's wallet to the escrow wallet.
+    token::transfer(
+        ctx.accounts.token_program.clone(),
+        &Transfer {
+            from: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
+            to: ctx.accounts.escrow_wallet_state.to_account_info(),
+            authority: ctx.accounts.user_sending.clone(),
+        },
+        amount,
+    )?;
 
     Ok(())
 }
--- programs/staking/src/instructions/withdraw_funds.rs
+++ programs/staking/src/instructions/withdraw_funds.rs
@@ -1,45 +1,48 @@
-use crate::errors::ErrorCode;
-use crate::state::State;
-use crate::utils::calculate_rewards;
-use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
+use crate::{
+    errors::ErrorCode,
+    state::State,
+    utils::calculate_rewards,
+};
+use anchor_lang::{
+    prelude::*,
+    solana_program::{program_error::ProgramError, system_instruction},
+};
+use anchor_spl::{
+    token::{self, Burn, Mint, Token, TokenAccount, Transfer},
+    utils::assert_initialized,
+};
 
 #[derive(Accounts)]
 #[instruction(application_idx: u64, state_bump: u8, wallet_bump: u8)]
 pub struct Withdraw<'info> {
-    #[account(
-        mut,
-        seeds = [b"state".as_ref(),
-                 user_sending.key().as_ref(),
-                 mint_of_token_being_sent.key().as_ref(),
-                 application_idx.to_le_bytes().as_ref()],
-        bump = state_bump
-    )]
+    #[account(mut, 
+        seeds = [b"state".as_ref(), 
+                 user_sending.key().as_ref(), 
+                 mint_of_token_being_sent.key().as_ref(), 
+                 application_idx.to_le_bytes().as_ref()], 
+        bump = state_bump)]
     application_state: Account<'info, State>,
     #[account(
         mut,
         seeds = [b"wallet".as_ref(),
-                mint_of_token_being_sent.key().as_ref(),
-                application_idx.to_le_bytes().as_ref()],
+                 mint_of_token_being_sent.key().as_ref(),
+                 application_idx.to_le_bytes().as_ref()],
         bump = wallet_bump,
-        token::mint=mint_of_token_being_sent,
-        token::authority=application_state,
+        token::mint = mint_of_token_being_sent,
+        token::authority = application_state,
     )]
     escrow_wallet_state: Account<'info, TokenAccount>,
     #[account(mut)]
     user_sending: Signer<'info>,
     mint_of_token_being_sent: Account<'info, Mint>,
-    #[account(
-        mut,
-        // constraint=refund_wallet.owner == user_sending.key(),
-        // constraint=refund_wallet.mint == mint_of_token_being_sent.key()
-    )]
+    #[account(mut, constraint = refund_wallet.owner == user_sending.key())]
     refund_wallet: Account<'info, TokenAccount>,
     system_program: Program<'info, System>,
     token_program: Program<'info, Token>,
     rent: Sysvar<'info, Rent>,
 }
 
+#[access_control(is_not_expired)]
 pub fn withdraw_funds_handler(
     ctx: Context<Withdraw>,
     application_idx: u64,
@@ -48,18 +51,18 @@ pub fn withdraw_funds_handler(
     current_timestamp: u64,
 ) -> Result<()> {
     let state = &mut ctx.accounts.application_state;
-    let _amount = state.amount_tokens;
 
+    // Ensure withdrawal request has been made
     if state.withdrawal_request_timestamp == 0 {
-        return Err(error!(ErrorCode::NoWithdrawalRequest));
-    };
+        return Err(ErrorCode::NoWithdrawalRequest.into());
+    }
 
+    // Ensure unbounding period has passed
     let days_since_withdrawal_request =
         (current_timestamp - state.withdrawal_request_timestamp) / (60 * 60 * 24 * 1000);
-
     if days_since_withdrawal_request < state.unbounding_period_in_days {
-        return Err(error!(ErrorCode::UnboundingPeriod));
-    };
+        return Err(ErrorCode::UnboundingPeriod.into());
+    }
 
     let total_amount = calculate_rewards(
         state.amount_tokens,
@@ -67,48 +70,64 @@ pub fn withdraw_funds_handler(
         current_timestamp,
         state.unbounding_period_in_days,
     );
-    msg!(&total_amount.to_string());
 
-    msg!("Initialized new Safe Refund instance for {}", total_amount);
-
-    // CPI time! we now need to call into the Token program to transfer our funds to the
-    // Escrow wallet. Our state account account is a PDA, which means that no private key
-    // exists for the corresponding public key and therefore this key was not signed in the original
-    // transaction. Our program is the only entity that can programmatically sign for the PDA
-    // and we can do this by specifying the PDA "derivation hash key" and using `CpiContext::new_with_signer()`.
-
-    // This specific step is very different compared to Ethereum. In Ethereum, accounts need to first set allowances towards
-    // a specific contract (like ZeroEx, Uniswap, Curve..) before the contract is able to withdraw funds. In this other case,
-    // the SafePay program can use Bob's signature to "authenticate" the `transfer()` instruction sent to the token contract.
-    let bump_vector = state_bump.to_le_bytes();
-    let mint_of_token_being_sent_pk = ctx.accounts.mint_of_token_being_sent.key().clone();
-    let application_idx_bytes = application_idx.to_le_bytes();
-    let inner = vec![
-        b"state".as_ref(),
-        ctx.accounts.user_sending.key.as_ref(),
-        mint_of_token_being_sent_pk.as_ref(),
-        application_idx_bytes.as_ref(),
-        bump_vector.as_ref(),
-    ];
-    let outer = vec![inner.as_slice()];
-
-    // Below is the actual instruction that we are going to send to the Token program.
-    let transfer_instruction = Transfer {
-        from: ctx.accounts.escrow_wallet_state.to_account_info(),
-        to: ctx.accounts.refund_wallet.to_account_info(),
-        authority: state.to_account_info(),
-    };
+    // Transfer funds from escrow wallet to refund wallet
     let cpi_ctx = CpiContext::new_with_signer(
         ctx.accounts.token_program.to_account_info(),
-        transfer_instruction,
-        outer.as_slice(),
+        Transfer {
+            from: ctx.accounts.escrow_wallet_state.to_account_info(),
+            to: ctx.accounts.refund_wallet.to_account_info(),
+            authority: ctx.accounts.application_state.to_account_info(),
+        },
+        vec![
+            b"state".as_ref(),
+            ctx.accounts.user_sending.key.as_ref(),
+            ctx.accounts.mint_of_token_being_sent.key().as_ref(),
+            application_idx.to_le_bytes().as_ref(),
+            &[state_bump],
+        ],
     );
+    token::transfer(cpi_ctx, total_amount)?;
 
-    // The `?` at the end will cause the function to return early in case of an error.
-    // This pattern is common in Rust.
-    anchor_spl::token::transfer(cpi_ctx, total_amount)?;
+    // Burn tokens from escrow wallet
+    let cpi_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        Burn {
+            mint: ctx.accounts.mint_of_token_being_sent.to_account_info(),
+            to: ctx.accounts.escrow_wallet_state.to_account_info(),
+            authority: ctx.accounts.application_state.to_account_info(),
+        },
+        vec![
+            b"state".as_ref(),
+            ctx.accounts.user_sending.key.as_ref(),
+            ctx.accounts.mint_of_token_being_sent.key().as_ref(),
+            application_idx.to_le_bytes().as_ref(),
+            &[state_bump],
+        ],
+    );
+    token::burn(cpi_ctx, total_amount)?;
 
+    // Reset amount_tokens in application state
     state.amount_tokens = 0;
+
+    Ok(())
+}
+
+fn is_not_expired(ctx: &Context<Withdraw>) -> Result<()> {
+    let current_timestamp = ctx.accounts.rent.context().slot;
+    let state = &ctx.accounts.application_state;
+
+    // Ensure withdrawal request has been made
+    if state.withdrawal_request_timestamp == 0 {
+        return Err(ErrorCode::NoWithdrawalRequest.into());
+    }
+
+    // Ensure unbounding period has passed
+    let days_since_withdrawal_request =
+        (current_timestamp - state.withdrawal_request_timestamp) / (60 * 60 * 24 * 1000);
+    if days_since_withdrawal_request < state.unbounding_period_in_days {
+        return Err(ErrorCode::UnboundingPeriod.into());
+    }
 
     Ok(())
 }
